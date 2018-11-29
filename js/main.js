0807dada8b6ef6fe38ebcc78eef4ce3acae1048c
Robert Klim <kontakt.robert.klim@gmail.com>
2018-11-29 17:46:56 +0100

    Create Perceptron class and initiate input weights randomly

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
+let perceptron;
+
 function setup() {
     createCanvas(600, 400);
+
+    perceptron = new Perceptron(2);
+
+    perceptron.printWeights();
 
 }
 
 function draw() {
     background(0);
+}
+
+class Perceptron {
+    constructor(weightsNum) {
+        this.weights = new Array(weightsNum);
+        this.initiateWeights();
+    }
+
+    initiateWeights() {
+        for (let i = 0; i < this.weights.length; i++) {
+            this.weights[i] = random(-1, 1);
+        }
+    }
+
+    printWeights() {
+        console.table(this.weights);
+    }
+
+    getWeight(i) {
+        return this.weights[i];
+    }
+
 }
