4f04f80091fc17743b612d2ee310d8279e1233a3
Robert Klim <kontakt.robert.klim@gmail.com>
2018-12-01 15:25:54 +0100

    3-Layer Neural Network - Initial commit

---
+++ js/NeuralNetwork.js
@@ -0,0 +1,7 @@
+class NeuralNetwork {
+    constructor(numInput, numHidden, numOutput) {
+        this.inpup_nodes = numInput;
+        this.hidden_nodes = numHidden;
+        this.output_nodes = numOutput;
+    }
+}
---
+++ js/sketch.js
@@ -0,0 +1,9 @@
+let nn;
+
+function setup() {
+    nn = new NeuralNetwork(3, 4, 2);
+}
+
+function draw() {
+
+}
