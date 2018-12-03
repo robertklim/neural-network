45be954eeda596b315434c046dc4585581808034
Robert Klim <kontakt.robert.klim@gmail.com>
2018-12-03 11:54:55 +0100

    Refactoring: change 'matrix' attribute name to 'data', add static methods, add print method

--- js/Matrix.js
+++ js/Matrix.js
@@ -2,11 +2,11 @@ class Matrix {
     constructor(rows, cols) {
         this.rows = rows;
         this.cols = cols;
-        this.matrix = [];
+        this.data = [];
         for (let i = 0; i < this.rows; i++) {
-            this.matrix[i] = [];
+            this.data[i] = [];
             for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] = 0;
+                this.data[i][j] = 0;
             }
         }
     }
@@ -14,7 +14,7 @@ class Matrix {
     randomize() {
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-                this.matrix[i][j] = Math.floor(Math.random() * 10);
+                this.data[i][j] = Math.floor(Math.random() * 10);
             }
         }
     }
@@ -24,64 +24,66 @@ class Matrix {
             // element wise add
             for (let i = 0; i < this.rows; i++) {
                 for (let j = 0; j < this.cols; j++) {
-                    this.matrix[i][j] += n.matrix[i][j];
+                    this.data[i][j] += n.data[i][j];
                 }
             }
         } else {
             // scalar wise add
             for (let i = 0; i < this.rows; i++) {
                 for (let j = 0; j < this.cols; j++) {
-                    this.matrix[i][j] += n;
+                    this.data[i][j] += n;
                 }
             }
         }
     }
 
-    transpose() {
-        let result = new Matrix(this.cols, this.rows);
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                result.matrix[j][i] = this.matrix[i][j];
+    static transpose(a) {
+        let result = new Matrix(a.cols, a.rows);
+        for (let i = 0; i < a.rows; i++) {
+            for (let j = 0; j < a.cols; j++) {
+                result.data[j][i] = a.data[i][j];
+            }
+        }
+        return result;
+    }
+
+    static multiply(a, b) {
+        // Matrix product
+        if (a.cols != b.rows) {
+            console.log('Columns of A must match rows of B');
+            return undefined;
+        }
+        let result = new Matrix(a.rows, b.cols);
+        for (let i = 0; i < result.rows; i++) {
+            for (let j = 0; j < result.cols; j++) {
+                // Dot product
+                let sum = 0;
+                for (let k = 0; k < a.cols; k++) {
+                    sum += a.data[i][k] * b.data[k][j];
+                }
+                result.data[i][j] = sum;
             }
         }
         return result;
     }
 
     multiply(n) {
-        if (n instanceof Matrix) {
-            // Matrix product
-            if (this.cols != n.rows) {
-                console.log('Columns of A must match rows of B');
-                return undefined;
-            }
-            let a = this;
-            let b = n;
-            let result = new Matrix(a.rows, b.cols);
-            for (let i = 0; i < result.rows; i++) {
-                for (let j = 0; j < result.cols; j++) {
-                    // Dot product
-                    let sum = 0;
-                    for (let k = 0; k < a.cols; k++) {
-                        sum += a.matrix[i][k] * b.matrix[k][j];
-                    }
-                    result.matrix[i][j] = sum;
-                }
-            }
-            return result;
-            // Hadamard product - element wise multiply
-            // for (let i = 0; i < this.rows; i++) {
-            //     for (let j = 0; j < this.cols; j++) {
-            //         this.matrix[i][j] *= n.matrix[i][j];
-            //     }
-            // }
-        } else {
-            // Scalar product - scalar wise multiply
-            for (let i = 0; i < this.rows; i++) {
-                for (let j = 0; j < this.cols; j++) {
-                    this.matrix[i][j] *= n;
-                }
+        // Hadamard product - element wise multiply
+        // for (let i = 0; i < this.rows; i++) {
+        //     for (let j = 0; j < this.cols; j++) {
+        //         this.data[i][j] *= n.data[i][j];
+        //     }
+        // }
+        // Scalar product - scalar wise multiply
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                this.data[i][j] *= n;
             }
         }
+    }
+
+    print() {
+        console.table(this.data);
     }
 
 }
--- js/sketch.js
+++ js/sketch.js
@@ -2,16 +2,16 @@ let nn;
 
 function setup() {
     nn = new NeuralNetwork(3, 4, 2);
-    let a = new Matrix(3, 3);
+    let a = new Matrix(2, 3);
     let b = new Matrix(3, 2);
     a.randomize();
     b.randomize();
-    console.table(a.matrix);
-    console.table(b.matrix);
-    let c = a.multiply(b);
-    console.table(c.matrix);
-    let d = a.transpose();
-    console.table(d.matrix);
+    a.print();
+    b.print();
+    let c = Matrix.multiply(a, b);
+    c.print();
+    let d = Matrix.transpose(c);
+    d.print();
 }
 
 function draw() {
